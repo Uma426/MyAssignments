b3db77ab7b70cfffdb8d5b8bb6e9c8659038ff13
Uma426 <umarajan2124@gmail.com>
2025-07-09 16:32:27 +0400

    Add files via upload
    Week1 day1 assignment

---
+++ week1/day1/ass1/Chrome.java
@@ -0,0 +1,32 @@
+package week1.day1.ass1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	/*1. version of type float with a value of 91.0f
+2. developer of type String with a value of "Google"
+3. isBeta of type boolean with a value of false
+4. releaseYear of type int with a value of 2008
+5. shortcutKey of type char with a value of 'C'
+c) Print the values of all variables using System.out.println.
+d) Run the program and confirm that the variable values
+are printed in the console.*/
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("The version is "+version);
+		System.out.println("The developer is "+developer);
+		System.out.println("Is beta "+isBeta);
+		System.out.println("The release Year is "+releaseYear);
+		System.out.println("The shortcut key is "+shortcutKey);
+
+
+
+	}
+
+}
