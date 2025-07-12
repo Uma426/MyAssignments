828a9bbf35fe7803b3ef6a0b044a9daf50f2c417
Uma426 <umarajan2124@gmail.com>
2025-07-12 15:21:22 +0400

    Add files via upload
    Week1 day 2 Class

---
+++ week1/day2/FindOddNumbers.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 7;
+		
+		if (a%2 ==0) {
+			System.out.println("The number is even");
+		}
+		else if(a%2!=0){
+			System.out.println("The number is odd");
+		}
+
+	}
+}
