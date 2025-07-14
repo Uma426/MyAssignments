2a8e7607f9278be9189f76821f4d74c3bff79653
Uma426 <umarajan2124@gmail.com>
2025-07-14 17:12:55 +0400

    Add files via upload
    Browser and edge browser--- Class assgnmnts

---
+++ week2/day1/EdgeBrowser.java
@@ -0,0 +1,14 @@
+package week2.day1;
+
+public class EdgeBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+       Browser b=new Browser();
+       System.out.println("The Browser is "+b.launchBrowser("Edge"));
+       b.loadUrl();
+
+	}
+
+}
