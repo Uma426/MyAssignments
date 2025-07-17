9f01184c1a09167b4208b51c6e7c8d730242bcd8
Uma426 <umarajan2124@gmail.com>
2025-07-17 20:12:16 +0400

    Add files via upload
    week2 day4 class (xpath and basic web driver methods)
    Flipkart application 
    week 2 day2 Login fb using edge browser

--- week2/day2/LoginEdge.java
+++ week2/day2/LoginEdge.java
@@ -1,13 +1,18 @@
 package week2.day2;
 
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import org.openqa.selenium.WebDriver;
+
 
 public class LoginEdge {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ChromeDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
+		System.setProperty("webdriver.edge.driver", "C:\\TESTING\\LearnSelenium\\Driver\\msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
+		
 		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
 	}
