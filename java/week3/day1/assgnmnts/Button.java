c8026eaa1baf8f84fbbb9a5868822aab083c6f26
Uma426 <umarajan2124@gmail.com>
2025-07-21 23:27:17 +0400

    Delete java/week3/day1/assgnmnts directory

--- java/week3/day1/assgnmnts/Button.java
+++
@@ -1,9 +0,0 @@
-package week3.day1.assgnmnts;
-
-public class Button extends WebElement {
-	
-	public void submit() {
-		System.out.println("Button Submitted");
-	}
-
-}
--- java/week3/day1/assgnmnts/TextField.java
+++
@@ -1,9 +0,0 @@
-package week3.day1.assgnmnts;
-
-public class TextField extends WebElement {
-	public String getText() {
-		
-		return "Text Field obtained";
-	}
-
-}
